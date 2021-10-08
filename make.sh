8affc38e12810dd2bdc864a4bf5ac141fdfb0c3e
Tiago <tiago.ribeiro@serpro.gov.br>
2021-10-08 15:05:10 -0300

    add arquivos

---
+++ make.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+ 
+apt-get update
+ 
+apt-get install -y \
+  build-essential \
+  curl \
+  git \
+  gzip \
+  libedit-dev \
+  libjansson-dev \
+  libldap2-dev \
+  libncurses5-dev \
+  libsqlite3-dev \
+  libsrtp2-dev \
+  libssl-dev \
+  libuuid1 \
+  libxml2-dev \
+  odbc-postgresql \
+  openssl \
+  postgresql-client \
+  sqlite3 \
+  tar \
+  unixodbc \
+  unixodbc-dev \
+  uuid-dev \
+  wget
+ 
+#git clone https://github.com/asterisk/asterisk.git /asterisk
+wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-16-current.tar.gz
+tar -xvf asterisk-*
+cd /asterisk-*
+contrib/scripts/install_prereq install
+./configure --with-pjproject-bundled
+make menuselect
+make
+make install
+make config
+make samples
+ 
+cd /tmp
+wget https://downloads.digium.com/pub/telephony/codec_opus/asterisk-16.0/x86-64/codec_opus-16.0_current-x86_64.tar.gz
+tar xvzf codec_opus-*
+cp codec_opus-*/*.so /usr/lib/asterisk/modules/
+cp codec_opus-*/codec_opus_config-en_US.xml /var/lib/asterisk/documentation/thirdparty
