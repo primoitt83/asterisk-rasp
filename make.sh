595c7d724589cc56dc48c5d3e442549f4875c6fe
Tiago <tiago.ribeiro@serpro.gov.br>
2021-10-22 14:45:07 -0300

    adicionado codec opus no menuselect

--- make.sh
+++ make.sh
@@ -30,25 +30,24 @@ apt-get install -y \
 #wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-16-current.tar.gz
 wget http://downloads.asterisk.org/pub/telephony/asterisk/old-releases/asterisk-13.33.0.tar.gz
 tar -xvf asterisk-*
+# Comente daqui pra baixo para alterar coisas no asterisk
 cd asterisk-13.33.0
 export DEBIAN_FRONTEND=noninteractive
 contrib/scripts/install_prereq install
 ./configure --with-pjproject-bundled
-
-
-# Comente daqui pra baixar para alterar coisas no asterisk
-# make menuselect.makeopts
-# menuselect/menuselect --disable-category MENUSELECT_CDR menuselect.makeopts
-# menuselect/menuselect --disable-category MENUSELECT_CEL menuselect.makeopts
-# menuselect/menuselect --enable MOH-OPSOUND-WAV menuselect.makeopts
-# menuselect/menuselect --disable-category MENUSELECT_UTILS menuselect.makeopts
-# menuselect/menuselect --disable astcanary --disable astdb2sqlite3 --disable astdb2bdb menuselect.makeopts
-# menuselect/menuselect --disable CORE-SOUNDS-EN-GSM menuselect.makeopts
-# menuselect/menuselect --enable CORE-SOUNDS-EN-WAV menuselect.makeopts
-# make -j4
-# make install
-# make config
-# make samples
+make menuselect.makeopts
+menuselect/menuselect --disable-category MENUSELECT_CDR menuselect.makeopts
+menuselect/menuselect --disable-category MENUSELECT_CEL menuselect.makeopts
+menuselect/menuselect --enable MOH-OPSOUND-WAV menuselect.makeopts
+menuselect/menuselect --disable-category MENUSELECT_UTILS menuselect.makeopts
+menuselect/menuselect --disable astcanary --disable astdb2sqlite3 --disable astdb2bdb menuselect.makeopts
+menuselect/menuselect --disable CORE-SOUNDS-EN-GSM menuselect.makeopts
+menuselect/menuselect --enable CORE-SOUNDS-EN-WAV menuselect.makeopts
+menuselect/menuselect --enable codec_opus menuselect.makeopts
+make -j4
+make install
+make config
+make samples
  
 #cd /tmp
 #wget https://downloads.digium.com/pub/telephony/codec_opus/asterisk-16.0/x86-64/codec_opus-16.0_current-x86_64.tar.gz
